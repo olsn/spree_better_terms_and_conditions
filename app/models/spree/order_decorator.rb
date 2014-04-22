2d758a766f0336c2dbc1f0bc0c15f216fceeb611
Aleks <ihunte@gmail.com>
2014-04-22 16:21:02 +0200

    Now supporting Spree 2-0-stable

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Order.class_eval do
 
-  # attr_accessible :terms_and_conditions # uncomment for Spree below version 2.1
+  attr_accessible :terms_and_conditions # uncomment for Spree below version 2.1
 
   # Add new checkout step to checkout process
   insert_checkout_step :terms_and_conditions, :before => :delivery
@@ -18,4 +18,4 @@ end
 Spree::Order.state_machine.before_transition :to => :delivery, :do => :valid_terms_and_conditions?
 
 # Add terms_and_conditions to strong parameters
-Spree::PermittedAttributes.checkout_attributes << :terms_and_conditions # Remove if Spree is below version 2.1
+# Spree::PermittedAttributes.checkout_attributes << :terms_and_conditions # Remove if Spree is below version 2.1
