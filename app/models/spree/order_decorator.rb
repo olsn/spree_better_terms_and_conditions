1e8cd2606e6d248f4b6ef85bf86aca7bc43ae9dd
Olaf Horstmann <oh@enpatech.de>
2014-05-13 13:26:20 +0200

    changed position of step

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,7 +3,7 @@ Spree::Order.class_eval do
   # attr_accessible :terms_and_conditions # uncomment for Spree below version 2.1
 
   # Add new checkout step to checkout process
-  insert_checkout_step :terms_and_conditions, :before => :delivery
+  insert_checkout_step :terms_and_conditions, :before => :confirm
 
   def valid_terms_and_conditions?
     unless terms_and_conditions == true
@@ -15,7 +15,7 @@ Spree::Order.class_eval do
 end
 
 # Validate on state change
-Spree::Order.state_machine.before_transition :to => :delivery, :do => :valid_terms_and_conditions?
+Spree::Order.state_machine.before_transition :to => :confirm, :do => :valid_terms_and_conditions?
 
 # Add terms_and_conditions to strong parameters
 Spree::PermittedAttributes.checkout_attributes << :terms_and_conditions # Remove if Spree is below version 2.1
