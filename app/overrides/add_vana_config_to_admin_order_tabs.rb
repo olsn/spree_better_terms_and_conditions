aa27f125194458a5bf33b9dfe527263b56a9c784
Olaf Horstmann <oh@enpatech.de>
2014-06-05 18:12:57 +0200

    fixed some bugs and added payment info output for adming

--- app/overrides/add_vana_config_to_admin_order_tabs.rb
+++ app/overrides/add_vana_config_to_admin_order_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
                      :name => "_vana_order_config",
                      :insert_before => "[data-hook='admin_order_edit_form']",
-                     :partial => "spree/admin/vana/_vana_order_config",
+                     :partial => "spree/admin/vana/vana_order_config",
                      :disabled => false)
