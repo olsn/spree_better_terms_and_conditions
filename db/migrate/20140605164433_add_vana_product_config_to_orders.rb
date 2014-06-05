bce81da8ae378852dd991a34a1684d80bea8d33f
Olaf Horstmann <oh@enpatech.de>
2014-06-05 17:41:08 +0200

    added vana config stuff

---
+++ db/migrate/20140605164433_add_vana_product_config_to_orders.rb
@@ -0,0 +1,6 @@
+class AddTermsAndConditionsToOrders < ActiveRecord::Migration
+  def change
+    add_column :spree_orders, :vana_product_config, :text
+    add_column :spree_orders, :vana_payment_info, :text
+  end
+end
