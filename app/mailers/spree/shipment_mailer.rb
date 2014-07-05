80be800d6b92903dea7ee9584b693f89ca4892ab
Malte Horstmann <horstmann@wi1.uni-hohenheim.de>
2014-07-05 14:17:34 +0200

    added shippment mailer

---
+++ app/mailers/spree/shipment_mailer.rb
@@ -0,0 +1,10 @@
+module Spree
+  class ShipmentMailer < BaseMailer
+    def shipped_email(shipment, resend = false)
+      @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
+      subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+      subject += "my-vana.de Versand-Bestaetigung fuer Bestellung ##{@shipment.order.number}"
+      mail(to: @shipment.order.email, from: from_address, subject: subject)
+    end
+  end
+end
