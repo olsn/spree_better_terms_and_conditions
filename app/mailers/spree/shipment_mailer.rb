b8caabe44e2ab902c690bd24dc69782693fd089c
Malte Horstmann <horstmann@wi1.uni-hohenheim.de>
2014-07-09 11:00:24 +0200

    changed attachment downloaded before send

--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -4,7 +4,8 @@ module Spree
       @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "my-vana.de Versand-Bestaetigung fuer Bestellung ##{@shipment.order.number}"
-      attachments['Rechnung.pdf'] = File.read('http://localhost:5433/admin/orders/'+@shipment.order.number+'.pdf?template=invoice')
+      file = open('http://localhost:5433/admin/orders/'+@shipment.order.number+'.pdf?template=invoice').read
+      attachments['Rechnung.pdf'] = file
       mail(to: @shipment.order.email, from: from_address, subject: subject)
     end
   end
