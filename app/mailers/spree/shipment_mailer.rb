4fce93183f0bbc3036b72e15989a48ee0137adad
Malte Horstmann <horstmann@wi1.uni-hohenheim.de>
2014-07-09 10:54:09 +0200

    changed PDF-DL adress

--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -4,7 +4,7 @@ module Spree
       @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
       subject += "my-vana.de Versand-Bestaetigung fuer Bestellung ##{@shipment.order.number}"
-      attachments['Rechnung.pdf'] = File.read('https://spree.my-vana.de/admin/orders/'+@shipment.order.number+'.pdf?template=invoice')
+      attachments['Rechnung.pdf'] = File.read('https://localhost:5433/admin/orders/'+@shipment.order.number+'.pdf?template=invoice')
       mail(to: @shipment.order.email, from: from_address, subject: subject)
     end
   end
